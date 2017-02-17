edaabcadb7e469da09c837c7255cb73a47444186
QuaintShanty <quaint.shanty@gmail.com>
2017-02-16 20:10:08 -0500

    Project hierarchy developed

---
+++ src/jse/Window.java
@@ -0,0 +1,30 @@
+package jse;
+
+public class Window {
+
+	public Window() {
+		initialize();
+	}
+	
+	/**
+	 * Initializes all of the components for the GUI
+	 */
+	public void initialize() {
+		// TODO Determine the size of all components below
+		// Forms the window for the GUI
+		
+
+		// Search box
+		
+
+		// Radio buttons for search type
+		
+
+		// Submit button
+		
+
+		// Text box for results
+		
+	}
+
+}
