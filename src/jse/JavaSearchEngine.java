6040f02b0df4da76476a66d41231284e5fd7dd06
QuaintShanty <quaint.shanty@gmail.com>
2017-02-17 09:28:18 -0500

    Instantiated the Window class

--- src/jse/JavaSearchEngine.java
+++ src/jse/JavaSearchEngine.java
@@ -8,13 +8,15 @@ package jse;
  *
  */
 public class JavaSearchEngine {
+	// Variables
+	static Window window;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		window = new Window();
 	}
 
 }
