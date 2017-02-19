559840dfe4d1eea257a3002ce3d7e8e1f20be324
QuaintShanty <quaint.shanty@gmail.com>
2017-02-18 23:22:05 -0500

    Created the window

--- src/jse/Window.java
+++ src/jse/Window.java
@@ -1,18 +1,47 @@
 package jse;
 
-public class Window {
+import java.awt.*;
+import javax.swing.JFrame;
 
+public class Window extends JFrame {
+
+	// Window Configuration Variables
+	int windowWidth = 650, windowHeight = 450;
+	final boolean RESIZABLE = false;
+	Insets insets;
+	
+	// User-Entered Variables
+	String query;
+	
+	// Enumerators
+	enum SearchTypes {
+		AND,
+		OR,
+		PHRASE
+	}
+	
+	/**
+	 * Constructor
+	 */
 	public Window() {
 		initialize();
 	}
 	
 	/**
-	 * Initializes all of the components for the GUI
+	 * Initializes all of the components for the window
 	 */
-	public void initialize() {
+	final void initialize() {
 		// TODO Determine the size of all components below
 		// Forms the window for the GUI
+		setTitle("JSE - Javanonymous");
+		setSize(windowWidth, windowHeight);
+		setResizable(RESIZABLE);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+		setFocusable(true);
 		
+		insets = getInsets();
+		setSize(insets.left + windowWidth + insets.right, insets.top + windowHeight + insets.bottom);
 
 		// Search box
 		
@@ -26,5 +55,4 @@ public class Window {
 		// Text box for results
 		
 	}
-
 }
