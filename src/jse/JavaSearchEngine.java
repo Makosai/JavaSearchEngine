1c0f24d0dc7bd56d624a6caa7e4e8e3af095a6de
Yosvany Reina <YosvanyReina@gmail.com>
2017-02-21 00:21:57 -0500

    Post-Cleanup Version 1.0.0

--- src/jse/JavaSearchEngine.java
+++ src/jse/JavaSearchEngine.java
@@ -4,42 +4,21 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import javax.swing.JPopupMenu;
+import javax.swing.*;
 import java.awt.Component;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import javax.swing.JButton;
-import javax.swing.SwingConstants;
-import java.awt.Dimension;
-import javax.swing.JTextField;
-import javax.swing.JRadioButton;
-import javax.swing.ButtonGroup;
-import javax.swing.JList;
-import javax.swing.JComboBox;
-import javax.swing.JTextPane;
-import javax.swing.JSeparator;
-import javax.swing.AbstractListModel;
-import javax.swing.ListSelectionModel;
-import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.JScrollPane;
 import javax.swing.border.BevelBorder;
-import java.awt.Window.Type;
 import java.awt.Color;
-import javax.swing.UIManager;
 import javax.swing.border.EtchedBorder;
 
 public class JavaSearchEngine extends JFrame {
 
-	private JPanel contentPane;
-	private JTextField textField;
-	private final ButtonGroup buttonGroup = new ButtonGroup();
-	private JTable table;
+	private JPanel windowPane;
+	private JTextField searchBar;
+	private final ButtonGroup radioButtonGroup = new ButtonGroup();
+	private JTable fileList;
 
 	/**
 	 * Launch the application.
@@ -58,7 +37,7 @@ public class JavaSearchEngine extends JFrame {
 	}
 
 	/**
-	 * Create the frame.
+	 * Create the window and contents.
 	 */
 	public JavaSearchEngine() {
 		setResizable(false);
@@ -66,86 +45,88 @@ public class JavaSearchEngine extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 650, 450);
 		
-		JMenuBar menuBar = new JMenuBar();
-		menuBar.setBorderPainted(false);
-		menuBar.setBackground(new Color(51, 51, 51));
-		setJMenuBar(menuBar);
+		JMenuBar searchEngineMenuBar = new JMenuBar();
+		searchEngineMenuBar.setBorderPainted(false);
+		searchEngineMenuBar.setBackground(new Color(51, 51, 51));
+		setJMenuBar(searchEngineMenuBar);
 		
-		JMenu mnFile = new JMenu("File");
-		mnFile.setBorderPainted(true);
-		mnFile.setBorder(new BevelBorder(BevelBorder.RAISED, new Color(102, 102, 102), new Color(102, 102, 102), new Color(0, 0, 0), new Color(0, 0, 0)));
-		mnFile.setForeground(new Color(255, 255, 255));
-		mnFile.setBackground(new Color(255, 255, 255));
-		menuBar.add(mnFile);
+		JMenu menuFile = new JMenu("File");
+		menuFile.setBorderPainted(true);
+		menuFile.setBorder(new BevelBorder(BevelBorder.RAISED, new Color(102, 102, 102), new Color(102, 102, 102), new Color(0, 0, 0), new Color(0, 0, 0)));
+		menuFile.setForeground(new Color(255, 255, 255));
+		menuFile.setBackground(new Color(255, 255, 255));
+		searchEngineMenuBar.add(menuFile);
 		
-		JMenuItem mntmAddFile = new JMenuItem("Add File(s)");
-		mntmAddFile.setBorderPainted(true);
-		mntmAddFile.setForeground(new Color(0, 0, 0));
-		mntmAddFile.setBackground(new Color(255, 255, 255));
-		mnFile.add(mntmAddFile);
+		JMenuItem subMenuAddFile = new JMenuItem("Add File(s)");
+		subMenuAddFile.setBorderPainted(true);
+		subMenuAddFile.setForeground(new Color(0, 0, 0));
+		subMenuAddFile.setBackground(new Color(255, 255, 255));
+		menuFile.add(subMenuAddFile);
 		
-		JMenuItem mntmRemoveFile = new JMenuItem("Remove File(s)");
-		mntmRemoveFile.setBorderPainted(true);
-		mntmRemoveFile.setForeground(new Color(0, 0, 0));
-		mntmRemoveFile.setBackground(new Color(255, 255, 255));
-		mnFile.add(mntmRemoveFile);
+		JMenuItem subMenuRemoveFile = new JMenuItem("Remove File(s)");
+		subMenuRemoveFile.setBorderPainted(true);
+		subMenuRemoveFile.setForeground(new Color(0, 0, 0));
+		subMenuRemoveFile.setBackground(new Color(255, 255, 255));
+		menuFile.add(subMenuRemoveFile);
 		
-		JMenuItem mntmRefreshFiles = new JMenuItem("Refresh File(s)");
-		mnFile.add(mntmRefreshFiles);
-		contentPane = new JPanel();
-		contentPane.setBackground(new Color(51, 51, 51));
-		contentPane.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		JMenuItem subMenuRefreshFile = new JMenuItem("Refresh File(s)");
+		menuFile.add(subMenuRefreshFile);
 		
-		textField = new JTextField();
-		textField.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		textField.setBounds(65, 40, 360, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		windowPane = new JPanel();
+		windowPane.setBackground(new Color(51, 51, 51));
+		windowPane.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		setContentPane(windowPane);
+		windowPane.setLayout(null);
 		
-		JRadioButton rdbtnAanyTerms = new JRadioButton("Any Terms");
-		rdbtnAanyTerms.setForeground(new Color(255, 255, 255));
-		rdbtnAanyTerms.setBackground(new Color(51, 51, 51));
-		buttonGroup.add(rdbtnAanyTerms);
-		rdbtnAanyTerms.setBounds(94, 86, 109, 23);
-		contentPane.add(rdbtnAanyTerms);
+		searchBar = new JTextField();
+		searchBar.setText("Please enter search terms...");
+		searchBar.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		searchBar.setBounds(65, 40, 360, 20);
+		windowPane.add(searchBar);
+		searchBar.setColumns(10);
 		
-		JRadioButton rdbtnExactPhrase = new JRadioButton("Exact Phrase");
-		rdbtnExactPhrase.setForeground(new Color(255, 255, 255));
-		rdbtnExactPhrase.setBackground(new Color(51, 51, 51));
-		buttonGroup.add(rdbtnExactPhrase);
-		rdbtnExactPhrase.setBounds(316, 86, 109, 23);
-		contentPane.add(rdbtnExactPhrase);
+		JRadioButton radioAnyTerms = new JRadioButton("Any Terms");
+		radioAnyTerms.setForeground(new Color(255, 255, 255));
+		radioAnyTerms.setBackground(new Color(51, 51, 51));
+		radioButtonGroup.add(radioAnyTerms);
+		radioAnyTerms.setBounds(94, 86, 109, 23);
+		windowPane.add(radioAnyTerms);
 		
-		JRadioButton rdbtnAllTerms = new JRadioButton("All Terms");
-		rdbtnAllTerms.setForeground(new Color(255, 255, 255));
-		rdbtnAllTerms.setBackground(new Color(51, 51, 51));
-		buttonGroup.add(rdbtnAllTerms);
-		rdbtnAllTerms.setBounds(205, 86, 109, 23);
-		contentPane.add(rdbtnAllTerms);
+		JRadioButton radioExactPhrase = new JRadioButton("Exact Phrase");
+		radioExactPhrase.setForeground(new Color(255, 255, 255));
+		radioExactPhrase.setBackground(new Color(51, 51, 51));
+		radioButtonGroup.add(radioExactPhrase);
+		radioExactPhrase.setBounds(316, 86, 109, 23);
+		windowPane.add(radioExactPhrase);
 		
-		JButton btnSearch = new JButton("Search");
-		btnSearch.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
-		btnSearch.setForeground(new Color(255, 255, 255));
-		btnSearch.setBackground(new Color(51, 51, 51));
-		btnSearch.setBounds(471, 39, 89, 23);
-		contentPane.add(btnSearch);
+		JRadioButton radioAllTerms = new JRadioButton("All Terms");
+		radioAllTerms.setForeground(new Color(255, 255, 255));
+		radioAllTerms.setBackground(new Color(51, 51, 51));
+		radioButtonGroup.add(radioAllTerms);
+		radioAllTerms.setBounds(205, 86, 109, 23);
+		windowPane.add(radioAllTerms);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		scrollPane.setBackground(new Color(51, 51, 51));
-		scrollPane.setBounds(10, 136, 624, 202);
-		contentPane.add(scrollPane);
+		JButton searchBtn = new JButton("Search");
+		searchBtn.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		searchBtn.setForeground(new Color(255, 255, 255));
+		searchBtn.setBackground(new Color(51, 51, 51));
+		searchBtn.setBounds(471, 39, 89, 23);
+		windowPane.add(searchBtn);
 		
-		table = new JTable();
-		table.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		table.setGridColor(new Color(255, 255, 255));
-		table.setBackground(new Color(51, 51, 51));
-		scrollPane.setViewportView(table);
-		table.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-		table.setName("Files\r\n");
-		table.setModel(new DefaultTableModel(
+		JScrollPane fileListScrollPane = new JScrollPane();
+		fileListScrollPane.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		fileListScrollPane.setBackground(new Color(51, 51, 51));
+		fileListScrollPane.setBounds(10, 136, 624, 202);
+		windowPane.add(fileListScrollPane);
+		
+		fileList = new JTable();
+		fileList.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		fileList.setGridColor(new Color(255, 255, 255));
+		fileList.setBackground(new Color(51, 51, 51));
+		fileListScrollPane.setViewportView(fileList);
+		fileList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		fileList.setName("Files\r\n");
+		fileList.setModel(new DefaultTableModel(
 			new Object[][] {
 				{null, null},
 				{null, null},
@@ -161,11 +142,11 @@ public class JavaSearchEngine extends JFrame {
 				{null, null},
 			},
 			new String[] {
-				"Files", "Current"
+				"Files", "Status"
 			}
 		));
 	}
-	private static void addPopup(Component component, final JPopupMenu popup) {
+	private static void fileMenuPopup(Component component, final JPopupMenu popup) {
 		component.addMouseListener(new MouseAdapter() {
 			public void mousePressed(MouseEvent e) {
 				if (e.isPopupTrigger()) {
