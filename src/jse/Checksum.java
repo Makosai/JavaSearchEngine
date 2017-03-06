3a8fc585290bfbe3a17ee537668de71d715fcec0
epigott <epigott@pylon17>
2017-03-06 05:03:50 -0500

    Beginning of Checksum class. Detects changes of files and updates index if neccessary.

---
+++ src/jse/Checksum.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jse;
+
+/**
+ *
+ * @author epigott
+ */
+// class to detect changes in file content and update index if found.
+public class Checksum {
+    
+}
